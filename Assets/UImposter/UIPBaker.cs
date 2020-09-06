8c2b063fb827497fca2a5f6a582f2af071736842
envyless <yym4789@hotmail.com>
2020-09-06 22:25:51 +0900

    ready for bake

---
+++ Assets/UImposter/UIPBaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPBaker : MonoBehaviour
+{
+    public GameObject target;
+    public string pathOut;
+
+    // bake target to Imposter Builboard
+    public void OnEnable()
+    {
+        // 해당 텍스쳐 만드는 로직
+    }
+}
